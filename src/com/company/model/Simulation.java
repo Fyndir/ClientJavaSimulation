73fbdf0cb6e1891cb1bdcfb5931e3ed34e4e9d33
fyndir <antoine.gamain@protonmail.com>
2019-12-16 17:09:55 +0100

    in progress

--- src/com/company/model/Simulation.java
+++ src/com/company/model/Simulation.java
@@ -1,6 +1,7 @@
 package com.company.model;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Simulation {
 
@@ -8,21 +9,37 @@ public class Simulation {
 
     public List<Camion> mesCamions;
 
-    public void InitSimulation() {
-
-    }
-
     public Simulation(List<Capteur> mesCapteurs, List<Camion> mesCamions) {
         this.mesCapteurs = mesCapteurs;
         this.mesCamions = mesCamions;
     }
 
-    public void run() {
+    public void run() throws InterruptedException {
+        while (true) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (Exception e) {
+                System.out.println("erreur de sleep");
+            }
 
+            Refresh();
+
+            for (Camion camion : mesCamions) {
+                camion.simulation();
+            }
+            for (Capteur capteur : mesCapteurs) {
+                capteur.simulation();
+            }
+            this.UpdateDb();
+        }
     }
 
     private void UpdateDb() {
+        // generer a partir des deux listes des object json et les injecter dans les deux url que tom me fourni
+    }
 
+    private void Refresh(){
+        // met a jour dp les urls des serveurs
     }
 
 
